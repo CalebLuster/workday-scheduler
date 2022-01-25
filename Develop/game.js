ea92fb10f40caba25328248f9c3e5499c967bbb7
Caleb Luster <cwl117@outlook.com>
2022-01-25 14:15:07 -0600

    added pseudo code

--- Develop/game.js
+++ Develop/game.js
@@ -1,1 +1,3 @@
-const schedule = $(".container");
+// 1. Display date and time that updates regularly.
+// 2. Save items in local storage
+// 3. Have the times be color codied for the specific time that it is
