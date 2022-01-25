d26f8f97aea6a8bbf454773c0ba108f71970e225
Caleb Luster <cwl117@outlook.com>
2022-01-25 15:22:50 -0600

    hopefully the this color coding thing works... added color coding function.

--- Develop/game.js
+++ Develop/game.js
@@ -8,8 +8,30 @@ $("#current-time3").value(localStorage.getItem("current-time3"));
 $("#current-time4").value(localStorage.getItem("current-time4"));
 $("#current-time5").value(localStorage.getItem("current-time5"));
 
+function currentTime () {
+    let clock = moment().hour();
 
+    $(".time-block").each(function () {
+        let clockTimes = parseInt($(this).attribute("id").split("hour")[1]);
+
+        if (clockTimes === clock) {
+            $(this).addClass("present");
+            $(this).removeClass("future");
+            $(this).removeClass("past");
+        }
+        if (clockTimes < clock) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        }
+        if (clockTimes > clock) {
+            $(this).addClass("future");
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+        }
+    })
+}
 
 // 1. Display date and time that updates regularly.
-// 2. Save items in local storage
-// 3. Have the times be color codied for the specific time that it is
+// 2. Save items in local storage. ✅
+// 3. Have the times be color codied for the specific time that it is✅
