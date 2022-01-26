fb7b399a5f8311f9c51d7796f3e7c3ea0228a59e
Caleb Luster <cwl117@outlook.com>
2022-01-25 18:15:20 -0600

    got it workinggit add . need to add readme file still

--- Develop/game.js
+++ Develop/game.js
@@ -1,47 +1,51 @@
-$("#current-time9").value(localStorage.getItem("current-time9"));
-$("#current-time10").value(localStorage.getItem("current-time10"));
-$("#current-time11").value(localStorage.getItem("current-time11"));
-$("#current-time12").value(localStorage.getItem("current-time12"));
-$("#current-time1").value(localStorage.getItem("current-time1"));
-$("#current-time2").value(localStorage.getItem("current-time2"));
-$("#current-time3").value(localStorage.getItem("current-time3"));
-$("#current-time4").value(localStorage.getItem("current-time4"));
-$("#current-time5").value(localStorage.getItem("current-time5"));
 
-let currentDate = moment().format("dddd, mmm, YYYY");
-$("#currentDay").html(todayDate);
 
-function currentTime () {
-    let clock = moment().hour();
-
-    $(".time-block").each(function () {
-        let clockTimes = parseInt($(this).attribute("id").split("hour")[1]);
-
-        if (clockTimes === clock) {
-            $(this).addClass("present");
-            $(this).removeClass("future");
-            $(this).removeClass("past");
-        }
-        if (clockTimes < clock) {
-            $(this).addClass("past");
-            $(this).removeClass("future");
-            $(this).removeClass("present");
-        }
-        if (clockTimes > clock) {
-            $(this).addClass("future");
-            $(this).removeClass("past");
-            $(this).removeClass("present");
-        }
-    })
-}
+let currentDate = moment().format("dddd, MMM do YYYY");
+$("#currentDay").html(currentDate);
 
 $(document).ready(function () {
     $(".btn").on("click", function () {
-        let userPrompt = $(this).siblings(".description").value();
-        let nineToFive = $(this).parent().attribute("id");
+        let userPrompt = $(this).siblings(".description").val();
+        let nineToFive = $(this).parent().attr("id");
         localStorage.setItem(userPrompt, nineToFive);
     })
+
+    function currentTime () {
+        let clock = moment().hour();
+        // console.log(clock)
+
+        $(".time-block").each(function () {
+            let clockTimes = parseInt($(this).attr("id"));
+            console.log(clockTimes)
+            if (clockTimes === clock) {
+                $(this).addClass("present");
+                $(this).removeClass("future");
+                $(this).removeClass("past");
+            }
+            if (clockTimes < clock) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+            }
+            if (clockTimes > clock) {
+                $(this).addClass("future");
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+            }
+        })
+    }
+        $("#9 .description").val(localStorage.getItem("current-time9"));
+        $("#10 .description").val(localStorage.getItem("current-time10"));
+        $("#11 .description").val(localStorage.getItem("current-time11"));
+        $("#12 .description").val(localStorage.getItem("current-time12"));
+        $("#13 .description").val(localStorage.getItem("current-time1"));
+        $("#14 .description").val(localStorage.getItem("current-time2"));
+        $("#15 .description").val(localStorage.getItem("current-time3"));
+        $("#16 .description").val(localStorage.getItem("current-time4"));
+        $("#17 .description").val(localStorage.getItem("current-time5"));
+    currentTime();
 })
-// 1. Display date and time that updates regularly.
+
+// 1. Display date and time that updates regularly.✅
 // 2. Save items in local storage. ✅
 // 3. Have the times be color codied for the specific time that it is✅
